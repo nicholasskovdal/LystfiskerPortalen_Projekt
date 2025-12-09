601392611f430c637ba696ef82644c63002d4ba3
Nicholas <nbsk67234@edu.ucl.dk>
2025-12-09 17:25:09 +0100

    Created some ViewModels instead of instantiating Entity Model directly in .razor page

---
+++ LystfiskerPortalen/UI/ViewModels/ChannelViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace LystfiskerPortalen.UI.ViewModels
+{
+    public class ChannelViewModel
+    {
+        public string ChannelId { get; set; }
+        public string Name { get; set; }
+        public IEnumerable<PostViewModel> Posts { get; set; } = new List<PostViewModel>();
+    }
+}
---
+++ LystfiskerPortalen/UI/ViewModels/PostViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace LystfiskerPortalen.UI.ViewModels
+{
+    public class PostViewModel
+    {
+        public string PostId { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public string Bait { get; set; }
+
+        public string Location { get; set; }
+        public string Technique { get; set; }
+        public string ImageUrl { get; set; }
+        public string Inspiration { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
